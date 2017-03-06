1987a40b4796ea3853e6d7125d29cc7827cda91c
tm4s <tmas.szanto@gmail.com>
2017-03-06 15:25:12 +0100

    add tachograph test

---
+++ train-tachograph/src/main/java/hu/bme/mit/train/tachograph/TachographTest.java
@@ -0,0 +1,15 @@
+package hu.bme.mit.train.tachograph;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Thomas on 2017. 03. 06..
+ */
+public class TachographTest {
+
+    @org.junit.Test
+    public void testIsTableEmpty() throws Exception {
+
+
+    }
+}
