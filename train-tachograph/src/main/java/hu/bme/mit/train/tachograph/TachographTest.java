47834b2248c776bf534b06fb35f42925b906df6a
zsolti111 <nemzsolti@gmail.com>
2017-03-06 15:31:51 +0100

    Test init

--- train-tachograph/src/main/java/hu/bme/mit/train/tachograph/TachographTest.java
+++ train-tachograph/src/main/java/hu/bme/mit/train/tachograph/TachographTest.java
@@ -6,9 +6,26 @@ import static org.junit.Assert.*;
  * Created by Thomas on 2017. 03. 06..
  */
 public class TachographTest {
+    TrainController controller;
+    TrainSensor sensor;
+    TrainUser user;
+    Tachograph graph;
+
+    @Before
+    public void before() {
+        TrainSystem system = new TrainSystem();
+        controller = system.getController();
+        user = system.getUser();
+        graph = new Tachograph();
+        sensor.overrideSpeedLimit(50);
+    }
 
     @org.junit.Test
     public void testIsTableEmpty() throws Exception {
+        user.overrideJoystickPosition(5);
+
+        controller.followSpeed();
+        graph.
 
 
     }
